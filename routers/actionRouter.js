52b729ec07e023aedb55285d82b8ec0f6dbf6128
chrisdalao <chrisdalao@gmail.com>
2019-07-12 09:44:57 -0700

    POST api for action model completed

--- routers/actionRouter.js
+++ routers/actionRouter.js
@@ -15,5 +15,25 @@ router.get('/', (req, res) => {
 });
 
 
+router.post('/', (req, res) => {
+    const actionData = req.body;
+    if (!actionData) {
+        res.status(400).json({ message: "missing action data" });
+    } else if (!actionData.project_id || !actionData.description || !actionData.notes) {
+        res.status(400).json({ message: "please provide the required fields for project_id, description and notes" });
+    } else {
+        Action.insert(actionData)
+            .then(action => {
+                if (action) {
+                    res.status(201).json(actionData)
+                }
+            })
+            .catch(err => {
+                err = { error: "There was an error while saving the action to the database" };
+                res.status(500).json(err);
+            })
+    }
+});
+
 
 module.exports = router;
